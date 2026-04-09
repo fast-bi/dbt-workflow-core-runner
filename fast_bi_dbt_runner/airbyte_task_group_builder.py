0b76a84322516595872b0943d984c08edbf5c1a8
GediminasKr-BURGA <gediminas.kristopaitis@burga.com>
2026-04-09 14:30:02 +0300

    Update

--- fast_bi_dbt_runner/airbyte_task_group_builder.py
+++ fast_bi_dbt_runner/airbyte_task_group_builder.py
@@ -3,10 +3,10 @@ from datetime import datetime, timedelta
 
 import pytz
 import requests
-from airflow.hooks.base import BaseHook
+from airflow.sdk.bases.hook import BaseHook
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import BranchPythonOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.utils.task_group import TaskGroup
 
 
